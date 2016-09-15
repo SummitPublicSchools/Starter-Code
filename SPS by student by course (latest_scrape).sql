9a381d95a65519d97620a14f47b3082ca133e311
snagalingam <snagalingam@summitps.org>
2016-09-15 10:19:10 -0700

    Update SPS by student by course (latest_scrape).sql

--- SPS by student by course (latest_scrape).sql
+++ SPS by student by course (latest_scrape).sql
@@ -1,188 +1,126 @@
---------------------------------------------------------------------------------------------------------------------------------------------
-
-/*******************************************************************************************************************************************
-
-Summit Schools
-By Student By Course By Focus Area
-Uses latest_scrape tables
-
-Rationale for Group By:
-One course can have multiple teachers. To address this, teachers have been aggregated by course.
-
-********************************************************************************************************************************************/
-
---------------------------------------------------------------------------------------------------------------------------------------------
+--pulls latest student and course level information. includes demographics: don't share SPED data
 
 SELECT
-	-- School
-	school_id
-	, school
 
-	-- Student
-	, grade_level
-	, student_id
-	, student_last_name
-	, student_first_name
-	, ethnicity
-	, socioeconomic_status
-	, sped
-	, english_proficiency
+  -- Student Information
+  sites.name AS "Site",
+  students.grade_level AS "Grade Level",
+  students.school_id AS "Student ID",
+  students.last_name AS "Student Last Name",
+  students.first_name AS "Student First Name",
+  students.email AS "Student Email",
 
-	-- Mentor
-	, mentor
+ CASE
+                WHEN student_demographics.ethnicity IS NOT NULL THEN student_demographics.ethnicity
+                ELSE 'Not Specified'
+                END AS ethnicity
+            , CASE
+                WHEN student_demographics.socioeconomic_status = 'T' THEN 'Socioeconomically Disadvantaged'
+                WHEN student_demographics.socioeconomic_status = 'F' THEN 'Not Socioeconomically Disadvantaged'
+                ELSE 'Awaiting Paperwork'
+                END AS socioeconomic_status
+            , CASE
+                WHEN student_demographics.is_sped = 'T' THEN 'Special Education'
+                WHEN student_demographics.is_sped = 'F' THEN 'General Education'
+                ELSE 'Awaiting Paperwork'
+                END AS sped
+            , CASE
+                WHEN student_demographics.english_proficiency IS NOT NULL THEN student_demographics.english_proficiency
+                ELSE 'Awaiting Paperwork'
+                END AS english_proficiency,
 
-	-- Subject
-	, subject_id
-	, subject
 
-	-- Course
-	, course_id
-	, course
+  -- Mentor Information
+  mentors.last_name AS "Mentor Last Name",
+  mentors.first_name AS "Mentor First Name",
+  mentors.email AS "Mentor Email Address",
 
-	-- Teachers
-	, LISTAGG(teacher, '; ') WITHIN GROUP (ORDER BY teacher) AS teachers
+  -- SPED Information
+  sped_cases.dbid IS NOT NULL AS "Student Is Special Ed?",
+  case_managers.last_name AS "Case Manager Last Name",
+  case_managers.first_name AS "Case Manager First Name",
 
-	-- Focus Areas
-	, focus_area_id
-	, focus_area
+  -- Course, Teacher, Section Information
+  subjects.name AS "Subject",
+  courses.name AS "Course Name",
+  courses.grade_level AS "Course Grade Level",
+  courses.default_seventy_pcnt_score AS "Default 70 Percent Score",
+  courses.alternate_seventy_pcnt_score AS "Alternate 70 Percent Score",
+  sections.sis_id AS "Illuminate Section ID",
+  sections.name AS "Period Name",
+
+  -- Grades
+  course_assignments.target_letter_grade AS "Grade Goal",
+  course_assignments.letter_grade AS "Current Letter Grade",
+  course_assignments.overall_score AS "Overall Course Score",
+  course_assignments.power_expected_pcnt AS "Power FAs Expected %",
+
+  -- Cog Skill Information (course-level)
+  course_assignments.project_score AS "Cog Skill Percentage",
+  ROUND(course_assignments.raw_cog_skill_score,5) AS "Cog Skill Score",
+
+  -- Focus Areas: Power
+  course_assignments.power_num_mastered AS "Power FAs Mastered",
+  course_assignments.power_out_of AS "Total Power FAs in Course",
+  course_assignments.power_out_of - course_assignments.power_num_mastered AS "Power FAs Left",
+  course_assignments.power_num_behind AS "Power FAs Behind",
+  ROUND(course_assignments.power_expected,3) AS "Power FAs Expected by End of Year",
+  course_assignments.power_on_track AS "On Track to Pass All Power Focus Areas",
+
+  -- Focus Areas: Additional
+  course_assignments.addl_num_mastered AS "Additional FAs Mastered",
+  course_assignments.addl_out_of AS "Total Additional FAs in Course",
+  course_assignments.addl_out_of - course_assignments.addl_num_mastered AS "Additional FAs Left",
+  ROUND(course_assignments.addl_expected,3) AS "Additional FAs Expected by End of Year",
+
+  -- Projects
+  course_assignments.num_projects_overdue AS "Number of Projects Overdue",
+  course_assignments.num_projects_graded as "Number of Projects Graded",
+  course_assignments.num_projects_ungraded as "Number of Projects Ungraded",
+  course_assignments.num_projects_total AS "Total Number of Projects",
+  COALESCE(course_assignments.num_projects_overdue, 0) = 0 AND
+  COALESCE(course_assignments.project_score, 100) >= 85 AS "On Track for Projects"
+
 
 FROM
-	(
+  latest_scrape_students AS students
+	LEFT OUTER JOIN summit_student_demographics AS student_demographics
+		ON student_demographics.student_id = students.dbid
+  LEFT OUTER JOIN latest_scrape_sped_cases AS sped_cases
+    ON sped_cases.student_id = students.dbid
+		AND students.visibility = 'visible'
+  LEFT OUTER JOIN latest_scrape_teachers AS case_managers
+    ON case_managers.dbid = sped_cases.teacher_id
+  INNER JOIN latest_scrape_sites AS sites
+    ON sites.dbid = students.site_id
+  LEFT OUTER JOIN latest_scrape_teachers AS mentors
+    ON mentors.dbid = students.mentor_id
+  INNER JOIN latest_scrape_course_assignments AS course_assignments
+    ON course_assignments.student_id = students.dbid
+		AND course_assignments.visibility = 'visible'
+  INNER JOIN latest_scrape_courses AS courses
+    ON courses.dbid = course_assignments.course_id
+		AND courses.visibility = 'visible'
+  LEFT OUTER JOIN latest_scrape_subjects AS subjects
+    ON subjects.dbid = courses.subject_id
+  LEFT OUTER JOIN latest_scrape_course_assignment_sections AS course_assignment_sections
+    ON course_assignment_sections.course_assignment_id = course_assignments.dbid
+  INNER JOIN latest_scrape_sections AS sections
+    ON sections.dbid = course_assignment_sections.section_id
 
-		SELECT
-			-- School
-			schools.dbid AS school_id
-			, schools."name" AS school
 
-			-- Student
-			, students.grade_level AS grade_level
-			, students.school_id AS student_id
-			, students.last_name AS student_last_name
-			, students.first_name AS student_first_name
-			, CASE
-				WHEN student_demographics.ethnicity IS NOT NULL THEN student_demographics.ethnicity
-				ELSE 'Not Specified'
-				END AS ethnicity
-			, CASE
-				WHEN student_demographics.socioeconomic_status = 'T' THEN 'Socioeconomically Disadvantaged'
-				WHEN student_demographics.socioeconomic_status = 'F' THEN 'Not Socioeconomically Disadvantaged'
-				ELSE 'Awaiting Paperwork'
-				END AS socioeconomic_status
-			, CASE
-				WHEN student_demographics.is_sped = 'T' THEN 'Special Education'
-				WHEN student_demographics.is_sped = 'F' THEN 'General Education'
-				ELSE 'Awaiting Paperwork'
-				END AS sped
-			, CASE
-				WHEN student_demographics.english_proficiency IS NOT NULL THEN student_demographics.english_proficiency
-				ELSE 'Awaiting Paperwork'
-				END AS english_proficiency
+WHERE
+  sites.district_id = 1 AND
+  sites.name NOT IN ('Unknown Summit','SPS Tour') AND
+  subjects.core = TRUE AND
+  courses.academic_year = 2017 AND
+  course_assignments.as_of - CURRENT_DATE > -1 AND
+  students.still_enrolled = TRUE
 
-			-- Mentor
-			, mentors.last_name || ', ' || mentors.first_name AS mentor
-
-			-- Subject
-			, subjects.dbid AS subject_id
-			, subjects."name" AS subject
-
-			-- Course
-			, courses.dbid AS course_id
-			, courses."name" AS course
-
-			-- Teacher
-			, teachers.last_name || ', ' || teachers.first_name AS teacher
-
-			-- Focus Areas
-			, focus_areas.dbid AS focus_area_id
-			, focus_areas."name" AS focus_area
-
-		FROM 
-			latest_scrape_sites AS schools
-
-			-- Students
-			LEFT OUTER JOIN latest_scrape_students AS students
-				ON students.site_id = schools.dbid
-				AND students.visibility = 'visible'
-				AND students.still_enrolled IS TRUE
-			LEFT OUTER JOIN summit_student_demographics AS student_demographics
-				ON student_demographics.student_id = students.dbid
-
-			-- Mentors
-			LEFT OUTER JOIN latest_scrape_teachers AS mentors
-				ON mentors.dbid = students.mentor_id
-
-			-- Students --> Course Assignments --> Courses --> Subject
-			-- NOTE:  Used an INNER JOIN because we are only interested in courses where there are course assignments.
-			INNER JOIN latest_scrape_course_assignments AS course_assignments
-				ON course_assignments.student_id = students.dbid
-				AND course_assignments.visibility = 'visible'
-			INNER JOIN latest_scrape_courses AS courses
-				ON courses.dbid = course_assignments.course_id
-				AND courses.visibility = 'visible'
-				AND courses.academic_year = 2017
-			LEFT OUTER JOIN latest_scrape_subjects AS subjects
-				ON subjects.dbid = courses.subject_id
-
-			-- Course Assignments --> Sections --> Teachers
-			LEFT OUTER JOIN latest_scrape_course_assignment_sections AS course_assignment_sections
-				ON course_assignment_sections.course_assignment_id = course_assignments.dbid
-			LEFT OUTER JOIN latest_scrape_sections AS sections
-				ON sections.dbid = course_assignment_sections.section_id
-			LEFT OUTER JOIN latest_scrape_section_teachers AS section_teachers
-				ON section_teachers.section_id = sections.dbid
-				AND section_teachers.visibility = 'visible'
-			LEFT OUTER JOIN latest_scrape_teachers AS teachers
-				ON teachers.dbid = section_teachers.teacher_id
-				AND teachers.visibility = 'visible'
-
-			-- Courses --> Focus Areas
-			LEFT OUTER JOIN latest_scrape_course_know_dos AS course_focus_areas
-				ON course_focus_areas.course_id = courses.dbid
-			-- NOTE:  Used an INNER JOIN to eliminate courses without Focus Areas
-			INNER JOIN latest_scrape_know_dos AS focus_areas
-				ON focus_areas.dbid = course_focus_areas.know_do_id
-				AND focus_areas.visibility = 'visible'
-
-		WHERE
-			schools.enrollment_group = 'summit'
-			AND CAST(schools.sis_id AS integer) < 1000
-
-	)
-
-GROUP BY
-	-- School
-	school_id
-	, school
-
-	-- Student
-	, grade_level
-	, student_id
-	, student_last_name
-	, student_first_name
-	, ethnicity
-	, socioeconomic_status
-	, sped
-	, english_proficiency
-
-	-- Mentor
-	, mentor
-
-	-- Subject
-	, subject_id
-	, subject
-
-	-- Course
-	, course_id
-	, course
-
-	-- Focus Areas
-	, focus_area_id
-	, focus_area
 
 ORDER BY
-	school_id ASC
-	, grade_level ASC
-	, student_id ASC
-	, course_id ASC
-	, focus_area_id ASC
-;
+  "Site",
+  "Grade Level",
+  "Student Last Name",
+  "Student First Name"
+  ;
